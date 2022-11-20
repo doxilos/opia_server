22a06260c90c1090fa31363c71f804d1c9d66e14
doxilos <nurbakivural@gmail.com>
2022-11-20 14:11:41 +0300

    init

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+        const isCustomAuth = token.length < 212
+
+        let decodedData
+
+        if (token && isCustomAuth) {
+            decodedData = jwt.verify(token, process.env.JWT_SECRET)
+
+            req.userId = decodedData?.id
+        } else {
+            decodedData = jwt.decode(token)
+            req.userId = decodedData?.sub
+        }
+
+        next()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default auth
